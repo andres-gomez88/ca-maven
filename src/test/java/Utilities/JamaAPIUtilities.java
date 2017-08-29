36b252f961338b5583563327f7b730c4e4da771a
andres.gomez <andres.gomez@reflexionhealth.comgit config --global user.email andres.gomez@reflexionhealth.comgit config --global user.email andres.gomez@reflexionhealth.com>
2017-08-29 11:31:07 -0700

    Added JAMA API calls to LoginTestCases and JamaAPIUtilities class

---
+++ src/test/java/Utilities/JamaAPIUtilities.java
@@ -0,0 +1,44 @@
+
+package Utilities;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONObject;
+
+
+public class JamaAPIUtilities {
+    
+    private static String jamaUrl = "https://reflexionhealth.jamacloud.com/rest/latest/testruns/";
+    private static JSONObject jamaBody = new JSONObject();
+    private static JSONObject fields = new JSONObject();
+    private static JSONObject status = new JSONObject();
+    private static JSONObject jamaRes = new JSONObject();
+    
+    public static void testCaseJamaApiPass(int apiId) throws UnirestException {
+        status.put("status", "PASSED");
+        jamaBody.put("fields", fields);
+        fields.put("testRunSteps", new JSONObject[] { status });
+        HttpResponse<JsonNode> jamaReq = Unirest.put(jamaUrl + apiId)
+                .header("content-type", "application/json")
+                .header("authorization", "Basic YW5kcmVzLmdvbWV6Ojg4QWd6Xl9e")
+                .body(jamaBody)
+                .asJson();
+        jamaRes = jamaReq.getBody().getObject();
+        System.out.println(jamaRes.toString(2));
+    }
+    
+    public static void testCaseJamaApiFail(int apiId) throws UnirestException {
+        status.put("status", "FAILED");
+        jamaBody.put("fields", fields);
+        fields.put("testRunSteps", new JSONObject[] { status });
+        HttpResponse<JsonNode> jamaReq = Unirest.put(jamaUrl + apiId)
+                .header("content-type", "application/json")
+                .header("authorization", "Basic YW5kcmVzLmdvbWV6Ojg4QWd6Xl9e")
+                .body(jamaBody)
+                .asJson();
+        jamaRes = jamaReq.getBody().getObject();
+        System.out.println(jamaRes.toString(2));
+    }
+}
