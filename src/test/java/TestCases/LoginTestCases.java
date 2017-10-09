c6f95f9243a4b436549b283ffb5692ccb215ba69
andres.gomez <andres.gomez@reflexionhealth.comgit config --global user.email andres.gomez@reflexionhealth.comgit config --global user.email andres.gomez@reflexionhealth.com>
2017-10-09 10:42:09 -0700

    Cleaned up more comments

--- src/test/java/TestCases/LoginTestCases.java
+++ src/test/java/TestCases/LoginTestCases.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package TestCases;
 
 import Utilities.ExtentFactory;
@@ -29,10 +25,8 @@ import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import pageObjects.*;
-/**
- *
- * @author Admin
- */
+
+
 public class LoginTestCases {
     private static final Logger log = LogManager.getLogger(LoginTestCases.class.getName());
     WebDriver driver;
