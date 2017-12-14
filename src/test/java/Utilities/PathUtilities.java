be89fe8d23d1c8f215b0eda88701f6358c82e672
andres.gomez <andres.gomez@reflexionhealth.comgit config --global user.email andres.gomez@reflexionhealth.comgit config --global user.email andres.gomez@reflexionhealth.com>
2017-12-14 11:50:17 -0800

    Added correct path for extent report config

--- src/test/java/Utilities/PathUtilities.java
+++ src/test/java/Utilities/PathUtilities.java
@@ -9,8 +9,8 @@ public class PathUtilities {
     private static String excelPath = "C:\\Users\\andres.gomez\\Documents\\NetBeansProjects\\CATest\\testData.xlsx";
     private static String extentRepPath = "C:\\Users\\andres.gomez\\Documents\\Screenshots\\";
     private static String extentConfPath = "C:\\Users\\andres.gomez\\Documents\\NetBeansProjects\\camaven\\src\\test\\resources\\extent-config.xml";
-    private static String extentRepPathMac = "/usr/local/reports/ca-maven";
-    private static String extentConfPathMac = "/users/qateam/netbeansprojects";
+    private static String extentRepPathMac = "/usr/local/reports/";
+    private static String extentConfPathMac = "/users/qateam/netbeansprojects/ca-maven/src/test/resources/extent-config.xml";
     private static String screenshotFFPath = "C:\\Users\\andres.gomez\\Documents\\Screenshots\\Firefox\\";
     private static String screenshotCHPath = "C:\\Users\\andres.gomez\\Documents\\Screenshots\\Chrome\\";
     private static String screenshotIEPath = "C:\\Users\\andres.gomez\\Documents\\Screenshots\\IExplorer\\";
